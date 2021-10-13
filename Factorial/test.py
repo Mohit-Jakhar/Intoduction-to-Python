d781a77a47c2b9640cf44f4adba237831aee7f26
Mohit Jakhar <87889989+Mohit-Jakhar@users.noreply.github.com>
2021-10-13 19:16:33 +0530

    Create test.py

---
+++ Factorial/test.py
@@ -0,0 +1,10 @@
+def fact(n):
+	if n==1:
+	       return n
+	else:
+	       return n*fact(n-1)
+  num=int(input(“Enter a number:”))
+  if num=0:
+	      print(“The factorial of 0 is 1.”)
+   else:
+	      print(“Factorial value of “num”, ”is”, fact(num))
